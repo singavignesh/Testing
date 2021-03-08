08b9dd48c6b1c4c99c74fba205bd5cb6e95cda2a
singavignesh <k.vignesh28@gmail.com>
2021-03-08 23:09:06 +0530

    started successfully

---
+++ src/org/tcs/Gods.java
@@ -0,0 +1,27 @@
+package org.tcs;
+
+public class Gods {
+	public static void method1() {
+		System.out.println("Thanks Singam Ayya");
+	}
+	public static void method2() {
+		System.out.println("Thanks Pechi amma");
+	}
+	public static void method3() {
+		System.out.println("Thanks Karupu appa");
+	}
+	public static void method4() {
+		System.out.println("Thanks Ujjaini amma");
+	}
+	public static void method5() {
+		System.out.println("Thanks Ayyapan appa");
+	}
+	public static void main(String[] args) {
+		method1();
+		method2();
+		method3();
+		method4();
+		method5();
+	}
+
+}
